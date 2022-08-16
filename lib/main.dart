b3a60628dbcbea4ee3bfcf011c19069e739f5889
KDH7751 <pnix7649@gmail.com>
2022-08-16 12:32:42 +0900

    3강 회원가입 페이지

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blog/pages/post/home_page.dart';
+import 'package:flutter_blog/pages/user/join_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: JoinPage(),
     );
   }
 }
