cdd785354b04a878551bec26511e9bccfad145e3
KDH7751 <pnix7649@gmail.com>
2022-08-16 16:13:08 +0900

    5강 GetX 페이지 이동

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blog/pages/post/home_page.dart';
 import 'package:flutter_blog/pages/user/join_page.dart';
 import 'package:flutter_blog/pages/user/login_page.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: JoinPage(),
     );
