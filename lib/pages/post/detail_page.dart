34213bbbfe729ac4fe8a37c19689c35d8c268904
KDH <dragon5354@coredax.com>
2022-12-16 16:48:40 +0900

    12강 상세보기 화면 완성하기

--- lib/pages/post/detail_page.dart
+++ lib/pages/post/detail_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/pages/post/home_page.dart';
+import 'package:flutter_blog/pages/post/update_page.dart';
 import 'package:get/get.dart';
 
 class DetailPage extends StatelessWidget {
@@ -12,8 +14,38 @@ class DetailPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Text('Detail Page $id'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '글 제목!!',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+            ),
+            Divider(),
+            Row(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Get.off(HomePage());
+                    },
+                    child: Text('삭제')),
+                SizedBox(width: 10),
+                ElevatedButton(
+                    onPressed: () {
+                      Get.to(UpdatePage());
+                    },
+                    child: Text('수정')),
+              ],
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text('글 내용!!' * 500),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
